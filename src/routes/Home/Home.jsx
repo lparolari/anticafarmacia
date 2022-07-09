a5c6524066a7915921cc00e7a6016f34ccc95a59
lparolari <luca.parolari23@gmail.com>
2022-07-09 22:59:46 +0200

    refactor: move imports

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,4 @@
-// Css
-import '../../assets/css/bootstrap.min.css' // import 'bootstrap/dist/css/bootstrap.min.css'
-import '../../assets/css/slick.css' // import 'slick-carousel/slick/slick.css'
-import '../../assets/css/magnific-popup.css' // import 'magnific-popup/dist/magnific-popup.css'
-import 'react-datepicker/dist/react-datepicker.css'
-import '../../assets/css/animate.min.css'
-import '../../assets/css/font-awesome.min.css'
-import '../../assets/css/flaticon.css'
-import '../../assets/css/default.css'
-import '../../assets/scss/style.scss'
+import '../../utils/imports'
 
 import Footer from '../../components/Footer/Footer'
 import { Header } from '../../components/Header/Header'
---
+++ src/utils/imports.js
@@ -0,0 +1,10 @@
+// Css
+import '../assets/css/bootstrap.min.css' // import 'bootstrap/dist/css/bootstrap.min.css'
+import '../assets/css/slick.css' // import 'slick-carousel/slick/slick.css'
+import '../assets/css/magnific-popup.css' // import 'magnific-popup/dist/magnific-popup.css'
+import 'react-datepicker/dist/react-datepicker.css'
+import '../assets/css/animate.min.css'
+import '../assets/css/font-awesome.min.css'
+import '../assets/css/flaticon.css'
+import '../assets/css/default.css'
+import '../assets/scss/style.scss'
